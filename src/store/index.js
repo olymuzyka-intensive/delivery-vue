4d68b13a45b6a3a9f297e538a48bb157581b95d7
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-07-16 21:43:20 +0300

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+// store.js
+import { restArray } from '@/constants/rests'
+import { reactive } from 'vue'
+
+export const store = reactive({
+    isOpen: false,
+    rests: restArray,
+    toggleModal(value) {
+        this.isOpen = value
+    }
+})
---
+++ src/views/ProductView.vue
@@ -0,0 +1,45 @@
+<template>
+            <section class="products">
+                <div class="container">
+                    <div class="products-wrapper" id="eats-container">
+                        <div class="products-card"
+                        v-for="eat in eatArray"
+                        :key="eat.id"
+                        >
+                            <div class="products-card__image">
+                                <img :src="getUrl(eat.image)" alt="eat1">
+                            </div>
+                            <div class="products-card__description">
+                                <div class="products-card__description-row">
+                                    <h5 class="products-card__description--name">
+                                        {{eat.name}}
+                                    </h5>                                    
+                                </div>
+                                <div class="products-card__description-row">
+                                    <p class="products-card__description--text">
+                                        {{eat.info}}
+                                    </p>                                    
+                                </div>
+                                <div class="products-card__description-row">
+                                      <div class="products-card__description-controls">
+                                        <button class="btn btn-primary">
+                                            В корзину
+                                            <img src="../assets/img/icons/cart-white.svg" alt="cart"> 
+                                        </button>
+                                        <span class="products-card__description-controls--price">{{eat.price}} ₽</span>
+                                      </div>                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>            
+                
+</template>
+
+<script setup>
+import { eatArray } from '../constants/eats'
+const getUrl = (name) => {
+    return new URL(`../assets/img/goods/${name}`, import.meta.url)
+}
+</script>
